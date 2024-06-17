6b182f8c13ca9f20482dd9031b9b48a893ab2a24
Bris <Bris@nowhere.org>
2024-06-17 19:04:54 +0200

    fix: use user discord invite link

--- src/discord/invite-uri.ts
+++ src/discord/invite-uri.ts
@@ -2,5 +2,5 @@ import { Env } from "@src/env";
 
 export function inviteUri(env: Env) {
   const applicationId = env.DISCORD_APPLICATION_ID;
-  return `https://discord.com/oauth2/authorize?client_id=${applicationId}&scope=bot`;
+  return `https://discord.com/oauth2/authorize?client_id=${applicationId}`;
 }
